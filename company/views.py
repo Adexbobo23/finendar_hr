08417892c50991a2f007355f5c222a60e5c7f940
Adeoluwa <oliyideadeoluwajohn1@gmail.com>
2024-01-06 21:24:30 +0100

    Updated version

--- company/views.py
+++ company/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib.auth.decorators import login_required
 
-# Create your views here.
+
+@login_required(login_url='login')
 def company_dashboard(request):
-    return render(request, 'creator/creator_dashboard.html')
+    return render(request, 'creator/index.html')
